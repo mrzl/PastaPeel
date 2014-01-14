8b354653d05283e34a692220816c2122e105dd11
mrzl <lzrm@mrzl.net>
2014-01-14 14:22:15 +0100

    - adds keystone, so the grid can be distorted - adds a PGraphics object, where everything is drawn on, this is necessary since keystone requires this for the distortion - javadoc comments
    Signed-off-by: mrzl <lzrm@mrzl.net>

--- src/main/java/pp/PastaPeelPApplet.java
+++ src/main/java/pp/PastaPeelPApplet.java
@@ -1,6 +1,11 @@
 package pp;
 
+import deadpixel.keystone.CornerPinSurface;
+import deadpixel.keystone.Keystone;
 import processing.core.PApplet;
+import processing.core.PGraphics;
+import processing.core.PVector;
+import processing.opengl.PGraphics2D;
 
 import java.util.Calendar;
 
@@ -26,30 +31,55 @@ public class PastaPeelPApplet extends PApplet {
     private ControlFrame controlFrame;
     private boolean drawGrid;
 
+    private PGraphics buffer;
+    private Keystone ks;
+    private CornerPinSurface surface;
+
+    /**
+     * Callback method from processing. This is called once, when the program is started
+     */
     @Override
     public void setup() {
         size(800, 800, P2D);
-        grid = new Grid( this, 50, 50 );
-        linePool = new LinePool( this );
+        buffer = createGraphics( width, height, P2D );
+
+        grid = new Grid( this, buffer, 50, 50 );
+        linePool = new LinePool( this, buffer );
         colorChooser = new ColorChooser( this );
         selectedColorIndex = 0;
         drawGrid = true;
 
         controlFrame = ControlFrame.createControlFrame( this, "Controls", 400, 600 );
+
+
+        ks = new Keystone( this );
+        surface = ks.createCornerPinSurface( width, height, 30 );
+
     }
 
+    /**
+     * Callback method from processing. This is called as often as possible.
+     */
     @Override
     public void draw() {
-        if( savePdf ) {
-            beginRecord( PDF, currentFileNamePDF);
+        background( 0 );
+        buffer.beginDraw();
+        buffer.background( 127 );
+        buffer.endDraw();
+
+        if( this.frame != null ){
+            frame.setTitle( ( int ) ( this.frameRate ) + "fps" );
         }
-        background( 127 );
+        if( savePdf ) {
+            beginRecord(PDF, currentFileNamePDF);
+        }
 
         if( !savePdf ) {
             if( drawGrid ) {
                 grid.draw();
             }
-            grid.drawActiveBox();
+            PVector surfaceMouse = surface.getTransformedMouse();
+            grid.drawActiveBox( surfaceMouse );
         }
 
         linePool.drawLines( ( int )( grid.getCellWidth() ) );
@@ -57,13 +87,22 @@ public class PastaPeelPApplet extends PApplet {
             endRecord();
             savePdf = false;
         }
+
+        surface.render( buffer );
     }
 
+    /**
+     * Callback method from processing, whenever a mouse key is pressed
+     */
     @Override
     public void mousePressed() {
-        linePool.mousePressed( grid, colorChooser.getColor( selectedColorIndex ) );
+        PVector surfaceMouse = surface.getTransformedMouse();
+        linePool.mousePressed( grid, colorChooser.getColor( selectedColorIndex ), surfaceMouse );
     }
 
+    /**
+     * Callback method from processing, whenever a key is pressed
+     */
     @Override
      public void keyPressed() {
         String lastLinesFileName;
@@ -81,16 +120,28 @@ public class PastaPeelPApplet extends PApplet {
                 linePool.loadFromFile( lastLinesFileName );
                 System.out.println( "Loaded lines from " + lastLinesFileName );
                 break;
+            case 'c':
+                ks.toggleCalibration();
+                break;
         }
     }
 
+    /**
+     * Saves the currently drawn lines to a file.
+     *
+     * @param lastLinesFileName the filename of the .lml file
+     */
     protected void saveConfiguration( String lastLinesFileName ) {
         linePool.saveToFile( lastLinesFileName );
         System.out.println( "Saved lines to " +  lastLinesFileName);
     }
 
-
-     protected void loadConfiguration( String loadLinesFileName ) {
+    /**
+     * Loads a file containing a set of lines.
+     *
+     * @param loadLinesFileName the file name of the .lml file
+     */
+    protected void loadConfiguration( String loadLinesFileName ) {
         linePool.loadFromFile( loadLinesFileName );
         System.out.println( "Loaded lines from " + loadLinesFileName );
     }
@@ -98,51 +149,63 @@ public class PastaPeelPApplet extends PApplet {
     protected void clearLines() {
         linePool.clear();
     }
-    /*
-    Saves a PDF with the current output.
+    /**
+     * Saves a PDF with the current output.
      */
      private void savePDF() {
          savePdf = true;
          currentFileNamePDF = "pasta_peel-" + getTimeStamp() + ".pdf";
     }
 
-    /*
-    Saves a PNG with the current output.
+    /**
+     * Saves a PNG with the current output.
      */
     private void savePNG() {
         currentFileNamePNG = "pasta_peel-" + getTimeStamp() + ".png";
-        saveFrame(currentFileNamePNG);
+        saveFrame( currentFileNamePNG );
     }
 
-    /*
-    creates a String encoding the current timestamp
-    @return String the String containing the current timestamp
+    /**
+     * creates a String encoding the current timestamp
+
+     @return String the String containing the current timestamp
      */
     public String getTimeStamp() {
         Calendar now = Calendar.getInstance();
         return String.format( "%1$ty%1$tm%1$td_%1$tH%1$tM%1$tS", now );
     }
 
-    /*
-    The main function. starting this PApplet.
+    /**
+     * The main function. starting this PApplet.
      */
     public static void main( String[] args ) {
         // starting this PApplet
         PApplet.main( new String[ ] { "pp.PastaPeelPApplet" } );
     }
 
+    /**
+     * Draws a box with the currently selected color to the passed PApplet
+     *
+     * @param controlWindow the control-applet.
+     */
     public void drawCurrentColor( PApplet controlWindow ) {
         controlWindow.stroke( 255 );
         controlWindow.fill( colorChooser.getColor( selectedColorIndex ) );
         controlWindow.rect( 140, 30, 50, 50 );
     }
 
-    protected void randomColor( ) {
+    /**
+     * Selects the next color
+     */
+    protected void nextColor() {
         selectedColorIndex++;
         selectedColorIndex %= colorChooser.getColorCount();
         System.out.println( "Set selectedColorIndex to " + selectedColorIndex );
     }
 
+    /**
+     * Toggles the grid on/off
+     */
     protected void toggleGrid() {
         drawGrid = !drawGrid;
     }
